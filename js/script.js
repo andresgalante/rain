f4686c44a52f3f4450adcecdec395be1e56746ae
Miguel Galante <mgalante@gmail.com>
2016-01-04 17:23:34 -0300

    Fix location

--- js/script.js
+++ js/script.js
@@ -1,40 +1,45 @@
 jQuery(function($, undefined) {
 	// get curent location
-	var location = "Buenos Aires, Argentina";
 	if ("geolocation" in navigator) {
 		navigator.geolocation.getCurrentPosition(function(position) {
-			location = position.coords.latitude + ',' + position.coords.longitude;
+			loadWeather(position.coords.latitude + ',' + position.coords.longitude);
 		});
-	} 
+	}else{
+		loadWeather("Buenos Aires, Argentina");
+	}
 	
 	function loadWeather(location, woeid) {
 		$.simpleWeather({
 			location: location,
 			woeid: woeid,
 			success: function(weather) {
-				var city = weather.city;
-				$(".location").text("en " + city + ".");
+				$(".location").text("en " + weather.city + ".");
 
 				var wcode = '<img class="weathericon" src="weathericons/' + weather.code + '.svg">';
 				$(".weather-icon").html(wcode);
 
 				if (/^(1|3|4|5|6|9|10|11|12|14|35|37|38|39|40|45|46|47)$/.test(weather.code)) {
-					$(".text").text("Llueve")
+					$(".text").text("Llueve");
 					$("body").addClass("show");
 				} else {
-					$(".text").text("No llueve")
+					$(".text").text("No llueve");
 					$("body").addClass("show");
 				}
+				
+				setTimeout(function() {
+					loadWeather(location);
+				}, 60000);
 			},
 			error: function(error) {
 				$("#weather").html('<p>' + error + '</p>');
+				
+				setTimeout(function() {
+					loadWeather(location);
+				}, 60000);
 			}
 		});
+		
 	}
-	
-	loadWeather(location);
-	setInterval(function() {
-		loadWeather(location);
-	}, 60000);
+		
 
 });
